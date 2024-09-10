fefacfbb38abcc3ba5d64bc5d7b13dfc4cedc1c9
unknown <ifumin.hs@gmail.com>
2024-09-10 10:32:28 -0400

    DATABSE

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -86,8 +86,9 @@ WSGI_APPLICATION = 'portfolio_project.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-DATABASES ={
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASES = {
+    'default': dj_database_url.config(),
 }
 
 # Password validation
