52a83390f822dd4c6fb3e91e27d2b7dcfa379c14
unknown <ifumin.hs@gmail.com>
2024-09-10 10:35:57 -0400

    allowed host

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-6_3npjte+oyi80wg@m_vx$y*o=+$dv8@srnct1et4@x!vdh+)u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
