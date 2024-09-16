4539326bb389e554630f4bf084de6c1073299b58
unknown <ifumin.hs@gmail.com>
2024-09-16 15:18:28 -0400

    S3

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -129,8 +129,19 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend_new/build/static'),  # 更新后的路径
 ]
+# 安裝 storages 後，設定為默認的存儲後端
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-MEDIA_URL = '/media/'
+# AWS 配置
+AWS_ACCESS_KEY_ID = 'AKIA5V6I6V3GODHE4MFI'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'ru040708-portfolio'
+AWS_S3_REGION_NAME = 'us-east-2'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+
+# 媒體文件的 URL
+MEDIA_URL = f'https://ru040708-portfolio.s3.amazonaws.com/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
